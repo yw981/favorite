5b4c1eafd2e33c1a0241f9eb779a659123396ac8
Tony Yang <yw981@sina.com>
2016-02-01 20:45:54 +0800

    add a little

--- resources/views/favorite/index.blade.php
+++ resources/views/favorite/index.blade.php
@@ -6,7 +6,7 @@
         <h2>{{ $favorite->title }}</h2>
         <favorite>
             <div class="body">
-                <a href="">{{ $favorite->url }}</a>
+                <a href="{{ $favorite->url }}" target="_blank">{{ $favorite->url }}</a>
             </div>
         </favorite>
     @endforeach
